94ed46ce90671b1408b44c60540b826773092cc0
Adityag0243 <mr.adityag123@gmail.com>
2024-11-28 01:58:58 +0530

    update

--- usaco/DP/increasing_freq.cpp
+++ usaco/DP/increasing_freq.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define all(arr)        arr.begin(), arr.end()
 #define vi              vector<int>
 #define vvi             vector<vi>
-// #define int             int64_t
+#define int             int64_t
 #define yes             cout << "YES" << endl;
 #define no              cout << "NO" << endl;
 #define p(x)            cout << (x) << endl;
@@ -15,7 +15,7 @@ using namespace std;
 #define smv(arr)        accumulate(arr.begin(), arr.end(), 0LL)
 #define srt(arr)        sort(arr.begin(), arr.end())
 #define rev(arr)        reverse(all(arr))
-#define MOD2            998244353
+#define MOD2            1000000007
 #define x               first
 #define y               second
 #define gcd(a,b)        __gcd((a),(b))
@@ -24,44 +24,19 @@ using namespace std;
 #define rl(i,st,n)      for(int i=n-1;i>=st;i--)
 #define fastio          ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
+
+
+// wrong
 signed main(){
-    fastio
-    int q,k;
-    cin>>q>>k;
-    vector<int> dp(k+1,0);
-    dp[0] = 1;
-
-    while(q--){
-        char sign;
-        cin>>sign;
-        int a;
-        cin>>a;
-
-        if(sign == '+'){
-
-            rl(i,0,k+1){
-                if(i-a >= 0){
-                    dp[i] += dp[i-a];
-                    dp[i] %= MOD2;
-                }
-            }
-
-        }else{
-            l(i,a,k+1){
-                dp[i] -= dp[i-a];
-                dp[i] %= MOD2;
-            }
-        }
-
-        cout<<dp[k]<<endl;
+    int n, c;
+    cin >> n >> c;
+    vi v(n);
+    l(i,0,n) cin >> v[i];
+    map<int,int>mp;
+    int ans = 0;
+    l(i,0,n){
+        mp[v[i]]++;
+        ans = max(ans,mp[v[i]]);
     }
-
-
+    cout<<ans;
 }
-
-
-
-// if I am at i = 10 then It doesn't matter in what order any coin comes,
-// only thing matter is that what are the coin we have at the end of the ith operation and then find 
-// how manys to make K that's why that else part works
-
