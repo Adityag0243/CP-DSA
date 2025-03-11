467e5f8a24e214b17fae7ca59dd31bb067942456
Adityag0243 <mr.adityag123@gmail.com>
2025-03-11 16:30:11 +0530

    full fledged code

--- cpg24/9march/ApowerB.cpp
+++ cpg24/9march/ApowerB.cpp
@@ -1,17 +1,27 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// % 999 is just for avoiding integer overflow.....
+// % 999 is just for avoiding long longeger overflow.....
 // cnt is just to calculate no of operation we are doing 
 
-int cnt = 0;
-int fn(int a, int b){
+long long cnt = 0;
+long long fn(long long a, long long b){
     cnt++;
     if(b == 1) return a;
-    int midP = fn(a, b/2);
+    long long midP = fn(a, b/2);
 
-    if(b%2 == 0) return midP * midP % 999;
-    else return midP * midP * a % 999;
+    // it is important to first store the multiplication in an variable 
+    // to force the mutliplication process come up in the integer range 
+    midP = (midP * midP) % 999;
+
+    // multiplication process knows that it is going to store in a variable
+    // that mean at the end it will be in specified range 
+    // so it will keep continue the multiplication process even if it overflows somewhere in between the process
+    // on the other hand if we directly return (midP * midP ) % 999 if midP*midP overflow the range it will throw error
+    // it will not proceed further and not able to know that overflow value is going to modulo by 999 
+
+    if(b%2 == 0) return midP;
+    else return (midP * a) % 999;
 }
 
 int main(){
